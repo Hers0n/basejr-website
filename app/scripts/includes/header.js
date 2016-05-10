03d57a5082dcebd3bd2bb15b8cf31503c6b5a4b2
Rodrigo Rigo <dallrigo@gmail.com>
2016-05-11 00:27:31 +0530

    some stuff

--- app/scripts/includes/header.js
+++ app/scripts/includes/header.js
@@ -5,12 +5,22 @@ const Header = function () {
   const $btn = $('.js-header-menu-toggle').first();
 
   function init() {
-    $btn.on('click', toggleCallback);
+    $btn.on('click', expandCallback);
+
+    $(window).on('scroll', fixCallback);
   }
 
-  function toggleCallback() {
+  function expandCallback() {
     $header.toggleClass('header--expanded');
     $body.toggleClass('header--expanded');
+  }
+
+  function fixCallback() {
+    if ($(window).scrollTop() >= 100) {
+      $header.addClass('header--fixed');
+    } else {
+      $header.removeClass('header--fixed');
+    }
   }
 
   return {
